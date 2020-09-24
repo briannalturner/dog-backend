0319ce2a58451ae62dc7f62335bb99386d5f3a47
briannalturner <blturner3527@gmail.com>
2020-09-24 15:58:30 -0400

    finished

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -10,4 +10,29 @@ class DogsController < ApplicationController
         render json: dog
     end
 
+    def update
+        dog = Dog.find(params[:id])
+        dog.update(dog_params)
+        render json: dog
+    end
+
+    def create
+        dog = Dog.new
+        dog.update(dog_params)
+        render json: dog
+    end
+
+    def destroy
+        dog = Dog.find(params[:id])
+        dog.delete
+
+        render json: {message: 'dog deleted'}
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :age, :weight, :breed, :coat, :eyes, :image)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Dog.destroy_all
+
 Dog.find_or_create_by(name: 'Bob', age: 13, weight: 60, breed: 'Poodle', coat: 'white', eyes: 'brown', image: 'https://www.purinaproclub.com/sites/g/files/auxxlc346/files/styles/facebook_share/public/Resources_Dog%20Articles_Health_Line%20108_Thumb.jpg?itok=iS4AIMko')
 Dog.find_or_create_by(name: 'Mitzi', age: 4, weight: 20, breed: 'Yorkshire Terrier', coat: 'black/tan', eyes: 'brown', image:'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/01125312/Yorkshire-Terrier-standing-outdoors-on-a-sunny-day.jpg')
 Dog.find_or_create_by(name: 'Calico', age:2, weight:50,breed:'Labrador Retriever', coat:'yellow', eyes:'brown', image:'https://www.thelabradorsite.com/wp-content/uploads/2011/12/yellow-lab.jpg')
